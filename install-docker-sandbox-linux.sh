a340d7ff818ed28dc991ebde8d1d6a6063670d74
Stijn Klomp <stijnklomp1@hotmail.com>
2026-05-30 11:48:52 +0100

    Update Docker Sandbox install script for Linux OS

--- install-docker-sandbox-linux.sh
+++ install-docker-sandbox-linux.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/sbx-dist && cd ~/sbx-dist
 
-curl -L https://github.com/docker/sbx-releases/releases/download/v0.25.0/DockerSandboxes-linux.tar.gz -o sbx.tar.gz
+curl -L https://github.com/docker/sbx-releases/releases/download/v0.31.1/DockerSandboxes-linux.tar.gz -o sbx.tar.gz
 tar -xzvf sbx.tar.gz
 
 sudo cp ~/sbx-dist/docker-sbx/sbx /usr/local/bin/sbx
@@ -10,4 +10,6 @@ sudo chmod +x /usr/local/bin/sbx
 
 sudo mkdir -p /usr/lib/docker/sandboxes
 sudo cp ~/sbx-dist/docker-sbx/nerdbox-* /usr/lib/docker/sandboxes/
-sudo cp ~/sbx-dist/docker-sbx/libkrun.so /usr/lib/docker/sandboxes/
+sudo cp ~/sbx-dist/docker-sbx/libsailor.so /usr/lib/docker/sandboxes/
+sudo cp ~/sbx-dist/docker-sbx/containerd-shim-nerdbox-v1 /usr/lib/docker/sandboxes/
+sudo cp ~/sbx-dist/docker-sbx/mkfs.erofs /usr/lib/docker/sandboxes/
